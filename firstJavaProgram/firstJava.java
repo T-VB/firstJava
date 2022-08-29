6a20f5bd3b6853b0729ec07c4e550a292e3678cb
T-VB <104457968+T-VB@users.noreply.github.com>
2022-08-29 15:28:41 -0700

    Add files via upload

---
+++ firstJavaProgram/firstJava.java
@@ -0,0 +1,13 @@
+//file name must match class name (Test)
+//every file is a class*
+public class firstJava {
+    public static void main(String[] args){
+        //All code goes here
+        //This is the launching pad ('main' method), cannot run Java program without it
+        System.out.println("My name is Tyler Vander Boegh");
+        System.out.println("I am 32 years old");
+        System.out.println("I live in Monroe, WA");
+        //compile with cmd: javac NameOfFile.java - fix any syntax errors
+        //run program w/ cmd: java NameOfFile
+    }
+}
